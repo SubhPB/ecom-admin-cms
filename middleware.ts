0ae3f40ff97094a8946c506627160d3e8ecd5a7e
subh <harvirbuttar2917@gmail.com>
2024-05-14 14:26:18 -0700

    ...

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+// Byimaan
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
