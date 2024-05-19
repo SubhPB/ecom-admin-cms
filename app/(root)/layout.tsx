4768bcc76120c4b4bff2fa8f05205b0187842f13
subh <harvirbuttar2917@gmail.com>
2024-05-19 10:31:42 -0700

    ...

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,8 +4,6 @@ import { auth } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 
 import { LayoutPropTS } from "@/types/components/components";
-import {p} from '@/utils/functions/func';
-
 import prismadb from "@/lib/prismadb";
 
 export default async function SetupLayout({children}:LayoutPropTS){
