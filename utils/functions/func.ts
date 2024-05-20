fb9916ffff680f21d40414405105b91f94fa183e
subh <harvirbuttar2917@gmail.com>
2024-05-20 09:08:18 -0700

    new util functions

--- utils/functions/func.ts
+++ utils/functions/func.ts
@@ -5,3 +5,11 @@ export const p = console.log;
 export const copyToClipboard = (str: string) => {
     navigator.clipboard.writeText(str);
 }
+export const formatter = new Intl.NumberFormat('en-us', {
+    style: 'currency',
+    currency: 'USD',
+});
+
+export const capitalize = (str: string) => (str[0].toUpperCase() + str.slice(1).toLowerCase()) ?? '';
+export const pluralize = (str: string, es=false) => es ? str + 'es' : str + 's';
+export const singularize = (str: string, es=false) => str.slice(0,str.length - (es ? 2 : 1));
