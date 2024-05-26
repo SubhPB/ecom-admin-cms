0ada1f090b77e37a89d466a465ed4bd895dc3994
subh <harvirbuttar2917@gmail.com>
2024-05-26 12:27:39 -0700

    api endpoint to HTTP methods related to billboard

--- app/api/[storeId]/billboards/route.ts
+++ app/api/[storeId]/billboards/route.ts
@@ -1,33 +1,11 @@
 // Byimaan
 
 import { ParamsArgsTS } from "@/types/objects/objs";
-import { NextResponse } from "next/server";
-import { auth } from "@clerk/nextjs/server";
-import prismadb from "@/lib/prismadb";
-import { p } from "@/utils/functions/func";
+
+import { BillboardApiManager } from "@/utils/api/billboard-api-manager";
+
 
 export async function GET(req: Request, paramsArgs: ParamsArgsTS){
-
-    const {params: {storeId}} = paramsArgs;
-
-    try {
-        if (!storeId) return new NextResponse("Store ID is required", {status: 400})
-
-        const {userId} = auth();
-        if (!userId) return new NextResponse("Unauthenticated", {status: 401});
-
-        const billboards = await prismadb.billboard.findMany(
-            {
-                where: {
-                    storeId: storeId,
-                }
-            }
-        );
-
-        return NextResponse.json(billboards);
-        
-    } catch (error) {
-        p('[BILLBOARDS_GET]', error);
-        return new NextResponse('Internal error', {status: 500})
-    }
+    const apiManager = new BillboardApiManager(req, paramsArgs);
+    return await apiManager.GETALL();
 }
