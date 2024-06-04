b75ba83818ef71bd44ba48fdb1e05e975df5b6ec
subh <harvirbuttar2917@gmail.com>
2024-06-04 10:26:14 -0700

    ...

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,27 +1,14 @@
 // Byimaan
 
 import React from 'react';
-import { DataBox } from '@/components/routes/dashboard/dashboard-utils';
+import DashboardComp from './dashboard';
+import { LayoutParamsPropTS } from '@/types/components/components';
 
-function DashboardPage() {
+function AdminMainPage({params: {storeId}}: LayoutParamsPropTS) {
+
   return (
-    <div className="dashboard w-full my-2 text-black py-3 px-5">
-
-      <div className="page-title">
-        <h2 className='font-bold text-2xl'> Dashboard </h2>
-        <h4 className='text-xs font-light'>Overview of your store.</h4>
-      </div>
-      
-      <div className="dashboard-data-holder w-full my-2">
-        
-        <div className="data-boxes w-full ">
-          <DataBox  title='Total Revenue' data='$39.02'/>
-        </div>
-
-      </div>
-
-    </div>
+    <DashboardComp storeId={storeId || null}/>
   )
-}
+};
 
-export default DashboardPage
+export default AdminMainPage
